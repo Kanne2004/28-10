8b17509b32d056f5462f21f5a0fa2d69cf0d1147
Kanne <doantrungkien18@gmail.com>
2024-10-28 16:22:40 +0700

    update

---
+++ Assets/_asset/script/ButtonContinue.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonContinue : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        Debug.Log("Continue!");
+    }
+}
