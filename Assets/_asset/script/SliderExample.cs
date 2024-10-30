265b59ec027c00a09629043c38f41384890a2964
Kanne <doantrungkien18@gmail.com>
2024-10-30 16:44:00 +0700

    update hw

--- Assets/_asset/script/SliderExample.cs
+++ Assets/_asset/script/SliderExample.cs
@@ -6,16 +6,16 @@ using UnityEngine.UI;
 public class SliderExample : MonoBehaviour
 {
     public Slider mySlider;
+    float health = 70;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mySlider.value = health;
     }
 
     public void OnSliderValueChange(float value)
     {
         Debug.Log("Slider Value: " + value);
     }
-
 }
